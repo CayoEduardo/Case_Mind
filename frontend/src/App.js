de46317c38f70b738a88201ddc8dc77169fdf890
Cayo Eduardo <cayo.eduardo.silveira@gmail.com>
2020-11-17 00:02:42 -0300

    Front end criado com CRA. Limpeza básica e adição de pastas necessárias.

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import background from './assets/Fundo.png'
+
+function App() {
+  return (
+    <div className='App'>
+      <img
+        src={background}
+        style={{ width: '1832px', height: '1024px' }}
+        alt=''
+      />
+    </div>
+  )
+}
+
+export default App
